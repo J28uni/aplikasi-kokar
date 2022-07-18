c4494fbd6d51d904202c3659dfb9d805f0dc4750
Juni Ahmad <49421289+J28uni@users.noreply.github.com>
2022-07-18 14:10:23 +0700

    update geolokasi
    update kode geolokasi untuk simpen ke database

--- cariconfig.php
+++ cariconfig.php
@@ -4,14 +4,12 @@ require_once "../kokar/db/db.php";
 $nik = isset($_POST['nik']) ? $_POST['nik'] : "";
 $nama = isset($_POST['nama']) ? $_POST['nama'] : "";
 $nm_toko = isset($_POST['nm_toko']) ? $_POST['nm_toko'] : "";
-// $latitude = isset($_POST['latitude']) ? $_POST['latitude'] : "";
-// $longitude = isset($_POST['longitude']) ? $_POST['longitude'] : "";
-$current = isset($_POST['current']) ? $_POST['current'] : "";
+$latitude = isset($_POST['latitude']) ? $_POST['latitude'] : "";
+$longitude = isset($_POST['longitude']) ? $_POST['longitude'] : "";
 $waktu = date("Y-m-d H:i:s");
 $cekpoin = isset($_POST['cekpoin']) ? $_POST['cekpoin'] : "";
 
-// $sql = "INSERT INTO peta (nik, nama, nm_toko, latitude, longitude, waktu, cekpoin) VALUES ('$nik','$nama','$nm_toko','$latitude','$longitude','$waktu','$cekpoin')";
-$sql = "INSERT INTO peta (nik, nama, nm_toko, current, waktu, cekpoin) VALUES ('$nik','$nama','$nm_toko','$current','$waktu','$cekpoin')";
+$sql = "INSERT INTO peta (nik, nama, nm_toko, latitude, longitude, waktu, cekpoin) VALUES ('$nik','$nama','$nm_toko','$latitude','$longitude','$waktu','$cekpoin')";
 $exe = $db->query($sql);
 
 if ($exe) {
